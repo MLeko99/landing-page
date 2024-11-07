fc18799a17d9c9fcc8bb486bc07126a8beac785e
MLeko99 <mile.leko80@gmail.com>
2024-11-07 20:03:31 +0100

    Implemented navbar and initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-item">Home</div>
+        <div className="navbar-item">About</div>
+        <div className="navbar-item">Details</div>
+        <div className="navbar-item">Contact us</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
